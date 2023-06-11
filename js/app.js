37cf450430b57475be3c665c9835de62287ffd1d
MUKE-coder <gmukejohnbaptist@gmail.com>
2023-06-11 13:35:40 +0300

    completed hero section

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper("#homeSlider", {
+  loop: true,
+  effect: "fade",
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
